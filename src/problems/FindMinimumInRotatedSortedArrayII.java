7080e7d19aad4183f90bb85093b1bff2d1af6e9a
ls <bigsoy.ls@gmail.com>
2014-10-21 16:12:32 +0800

    Find Minimum In Rotated Sorted Array 1&2

---
+++ src/problems/FindMinimumInRotatedSortedArrayII.java
@@ -0,0 +1,25 @@
+package problems;
+
+public class FindMinimumInRotatedSortedArrayII {
+
+	private int binaryFind(int[] num, int l, int r) {
+		if(l == r)
+			return num[l];
+		if(num[l] < num[r])
+			return num[l];
+		int mid = (l+r)/2;
+		return Math.min(binaryFind(num, l, mid), binaryFind(num, mid+1, r));
+	}
+	
+	public int findMin(int[] num) {
+		if(num == null || num.length == 0)
+			return -1;
+		return binaryFind(num, 0, num.length-1);
+	}
+	
+	public static void main(String[] args) {
+		int [] num = {4, 5, 6, 7, 0, 1, 2};
+		int min = new FindMinimumInRotatedSortedArrayII().findMin(num);
+		System.out.println(min);
+	}
+}
